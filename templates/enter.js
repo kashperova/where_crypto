cf4c89e8aaaed15435105468e0102f782e036bd7
Danylo Musiiaka <dollarone98@gmail.com>
2023-02-02 22:31:45 +0200

    updfrontend

---
+++ templates/enter.js
@@ -0,0 +1,12 @@
+var input = document.getElementById(".btn");
+
+
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById(".btn").click();
+  }
+});
